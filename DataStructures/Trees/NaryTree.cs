80c2e3894c8c5981b15b370f013f216dc95273d4
Mikesh Mistry <mikeshmistry@hotmail.ca>
2021-04-17 21:05:51 -0400

    Added implementation for print and find methods

--- DataStructures/Trees/NaryTree.cs
+++ DataStructures/Trees/NaryTree.cs
@@ -88,10 +88,94 @@ namespace DataStructures.Trees
         {
             root = null;
         }
-        
-          
+
+        public NaryTreeNode<T> Find(T item)
+        {
+            //case of empty tree and item being null 
+            if (root == null || item == null)
+                return null;
+
+            var queue = new Queue<NaryTreeNode<T>>();
+            var found = false;
+
+            //current node when traversing the tree
+            NaryTreeNode<T> current = null;
+
+            //holds the value of the found item
+            NaryTreeNode<T> foundItem = null;
+
+            //add root to the queue
+            queue.Enqueue(root);
+
+            //while the queue is not empty and item is found
+            while (queue.Count() != 0 && !found)
+            {
+                //remove the first node in the queue
+                current = queue.Dequeue();
+
+                //current node is not null
+                if (current != null)
+                {
+                    //if found 
+                    if (current.Data.Equals(item))
+                        found = true;
+
+                    //not found at the current level so add its children
+                    else
+                    {
+                        //add its children if they have any to the queue
+                        if (current.ChildrenList != null)
+                        {
+                            foreach (var child in current.ChildrenList)
+                                queue.Enqueue(child);
+                        }
+                    }
+                }
+
+            }
+
+            return foundItem;
+        }
+
+        public void PrintTree()
+        {
+            if (root == null)
+                Console.WriteLine("Empty Tree");
+
+            var queue = new Queue<NaryTreeNode<T>>();
+
+            NaryTreeNode<T> current = null;
+
+            queue.Enqueue(root);
+
+            //while the queue is not empty 
+            while(queue.Count() !=0)
+            {
+                //remove the first node in the queue
+                current = queue.Dequeue();
+
+                //current node is not null
+                if(current !=null)
+                {
+                    //print it
+                    Console.Write(current.Data);
+
+                    //add its children if they have any to the queue
+                    if(current.ChildrenList !=null)
+                    {
+                        foreach (var child in current.ChildrenList)
+                            queue.Enqueue(child);
+                    }
+                }
+
+                Console.WriteLine();
+            }
+
+        }
+
+
         #endregion
 
-       
+
     }
 }
